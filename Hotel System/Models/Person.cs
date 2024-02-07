faa5cced65cbe16ad0f220be24edd4492070152d
Mohamed <my1379@fayoum.edu.eg>
2024-02-07 18:36:45 +0200

    Fix some issues

---
+++ Hotel System/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel_System.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; } = null!;
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        [MaxLength(14)] [MinLength(14)] public string NId { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+    }
+}
