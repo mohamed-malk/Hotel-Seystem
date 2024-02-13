6e58f410aa14708e58157eb145f1c974c2f9c7f1
Mohamed <my1379@fayoum.edu.eg>
2024-02-14 01:36:36 +0200

    Add Reservartion Repo

--- Hotel System/Services/RoomRepo.cs
+++ Hotel System/Services/RoomRepo.cs
@@ -14,79 +14,6 @@ namespace Hotel_System.Services
                 throw Exceptions.AlreadyExistException("Room");
 
             _dbContext.Rooms.Add(room);
-            _dbContext.SaveChangesAsync();
-        }
-
-        public bool ReserveRoom(int clientId, int roomId,
-            float amount, DateTime startDate, DateTime endDate)
-        {
-            Room? room = _dbContext.Rooms.Find(roomId);
-            if (room == null || !room.IsAvailable)
-            {
-                // Room not found or already occupied
-                return false;
-            }
-
-            if (amount > room.Price)
-                throw new Exception("Amount higher than Room Price");
-
-            Reservation reservation = new()
-            {
-                ClientId = clientId,
-                RoomId = roomId,
-                CheckInDate = startDate,
-                CheckOutDate = endDate
-            };
-            Payment payment = new()
-            {
-                ClientId = clientId,
-                Date = DateTime.Now,
-                Amount = amount,
-                TargetAmount = room.Price * reservation.DurationDays
-            };
-            _dbContext.Payments.Add(payment);
-            _dbContext.SaveChanges();
-
-            Payment? paymentDb = _dbContext.Payments
-                .SingleOrDefault(p => p.ClientId == clientId && p.Date == payment.Date);
-
-            if (paymentDb != null)
-                reservation.PaymentId = paymentDb.Id;
-            else throw new("Something Wrong !");
-
-            // Reserve the room
-            room.IsAvailable = false;
-
-
-            _dbContext.Reservations.Add(reservation);
-            _dbContext.Rooms.Update(room);
-            
-            _dbContext.SaveChanges();
-            return true;
-        }
-
-        public void CancelReservation(int clintId, int roomId,  DateTime date)
-        {
-            Reservation? reservation = _dbContext.Reservations
-                .Include(r => r.Payment)
-                .FirstOrDefault(r =>
-                    r.ClientId == clintId && r.RoomId == roomId
-                                          && r.ReservationDate == date);
-                
-            if (reservation == null)
-                throw Exceptions.NotFoundException("Reservation");
-
-            if (reservation.Payment.Rest != 0)
-                throw new Exception("Cant Remove this Reservation, " +
-                                    "because there are Rest Money");
-   
-            Room? room = _dbContext.Rooms.Find(reservation.RoomId);
-            if (room != null)
-                room.IsAvailable = true; // Mark the room as not occupied
-            else throw new Exception("Room Doesn't Exist, Something Wrong");
-
-            _dbContext.Rooms.Update(room);
-            _dbContext.Reservations.Remove(reservation);
             _dbContext.SaveChanges();
         }
 
@@ -97,27 +24,6 @@ namespace Hotel_System.Services
                 $"{room.Rate} " +
                 $"{room.Type} " +
                 $"{room.IsAvailable}").ToList();
-
-        public List<string> GetReservationList() =>
-            _dbContext.Reservations.Select(reservation =>
-                $"{reservation.ReservationDate}" +
-                $"{reservation.CheckInDate}" +
-                $"{reservation.CheckOutDate}" +
-                $"{reservation.DurationDays}" +
-                $"{reservation.ClientId}" +
-                $"{reservation.RoomId}" +
-                $"{reservation.PaymentId}").ToList();
-
-        public List<string> GetPaymentList() =>
-            _dbContext.Payments.Select(payment =>
-                $"{payment.Id}" +
-                $"{payment.Date}" +
-                $"{payment.Amount}" +
-                $"{payment.TargetAmount}" +
-                $"{payment.Rest}" +
-                $"{payment.ClientId}").ToList();
-
-
     }
 
 }
