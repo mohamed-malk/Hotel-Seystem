af92743319d80b0a56145d37409222db03ee5972
Mohamed <my1379@fayoum.edu.eg>
2024-02-14 08:29:50 +0200

    Add Migratiion to DB

--- Hotel System Back/Migrations/20240214062801_AddTransactionTable.Designer.cs
+++ Hotel System Back/Migrations/20240214062801_AddTransactionTable.Designer.cs
@@ -4,6 +4,7 @@ using Hotel_System_Back.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hotel_System_Back.Migrations
 {
     [DbContext(typeof(HotelDbContext))]
-    partial class HotelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240214062801_AddTransactionTable")]
+    partial class AddTransactionTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
