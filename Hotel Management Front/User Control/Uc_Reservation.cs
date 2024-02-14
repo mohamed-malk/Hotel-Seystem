65aaf917d7e7b40f678daeccd30dd36d83d68153
nairagamal <nairagemi75@gmail.com>
2024-02-14 22:05:31 +0200

    n

--- Hotel Management Front/User Control/Uc_Reservation.cs
+++ Hotel Management Front/User Control/Uc_Reservation.cs
@@ -12,9 +12,46 @@ namespace Hotel_System_Front.User_Control
 {
     public partial class Uc_Reservation : UserControl
     {
+        ReservationRepo reserve = new ReservationRepo();
+        RoomRepo    RoomReserve = new RoomRepo(); 
+        ClientRepo ClientRepo = new ClientRepo();
         public Uc_Reservation()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //     int clientId, int roomId,
+            //float amount, DateTime startDate, DateTime endDate
+
+            int clientid = int.Parse(comboBox2.SelectedItem.ToString());
+            int roomid = int.Parse(comboBox1.SelectedItem.ToString());
+            float amount = float.Parse(textBox2.Text);
+            DateTime startDate = dateTimePicker1.Value;
+            DateTime endDate = dateTimePicker2.Value;
+
+
+            reserve.ReserveRoom(clientid, roomid, amount, startDate, endDate);
+
+            MessageBox.Show(clientid.ToString());
+        }
+
+        private void Uc_Reservation_Load(object sender, EventArgs e)
+        {
+            var rooms = RoomReserve.GetAvailableRooms();
+            foreach(Room room in rooms)
+            {
+                comboBox1.Items.Add(room.Id);
+            }
+
+            var clients = ClientRepo.GetAll();
+            foreach( Client client in clients)
+            {
+                comboBox2.Items.Add(client.Id);
+            }
+            
+            
         }
     }
 }
